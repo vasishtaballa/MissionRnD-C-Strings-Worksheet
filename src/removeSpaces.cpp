1d6fca730d355b281ccebfcdadebd7a115b75427
vasishtaballa <vasishtaballa@missionrnd.com>
2016-03-15 01:17:12 +0530

    Submit Commit

--- src/removeSpaces.cpp
+++ src/removeSpaces.cpp
@@ -10,7 +10,20 @@ ERROR CASES: Return '\0' for invalid inputs.
 
 NOTES: Don't create new string.
 */
+#include<stdio.h>
 
-char removeSpaces(char *str) {
+char removeSpaces(char *str) 
+{
+	if (str == NULL)
+		return '\0';
+	if (str == "")
+		return '\0';
+	int i, len, p = 0;
+	for (len = 0; str[len] != '\0'; len++)
+	{
+		if (str[len] != ' ')
+			str[p++] = str[len];
+	}
+	str[p] = '\0';
 	return '\0';
 }
