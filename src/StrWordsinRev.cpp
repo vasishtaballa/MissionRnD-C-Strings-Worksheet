42654f0b2c462126258ca9d5c2cbaa2e0f0ab882
vasishtaballa <vasishtaballa@missionrnd.com>
2016-03-15 01:15:19 +0530

    Submit Commit

--- src/StrWordsinRev.cpp
+++ src/StrWordsinRev.cpp
@@ -11,7 +11,45 @@ NOTES: Don't create new string.
 */
 #include <Stdio.h>
 #include <string.h>
+void reverse_word(char *input, int start, int end);
 
-void str_words_in_rev(char *input, int len){
-	
+void str_words_in_rev(char *input, int len)
+{
+	int i = 0, j = len - 1, k = 0;
+	char temp;
+	while (i < j)
+	{
+		temp = input[i];
+		input[i] = input[j];
+		input[j] = temp;
+		i++;
+		j--;
+	}
+	for (i = 0; i < len; i++)
+	{
+		if (input[i] == ' ')
+		{
+			reverse_word(input, k, i - 1);
+			k = i + 1;
+		}
+		if (i == len - 1)
+		{
+			reverse_word(input, k, i);
+			k = i + 1;
+		}
+	}
+}
+
+void reverse_word(char *input, int start, int end)
+{
+	int i = start, j = end;
+	char temp;
+	while (i < j)
+	{
+		temp = input[i];
+		input[i] = input[j];
+		input[j] = temp;
+		i++;
+		j--;
+	}
 }
